7e1da9773555fd19a236c0b0a65f31494bfbcc54
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-07 09:44:58 -0700

    set up config files and routes. initialize controllers and views for users. create home controller and views.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  # before_action :only => [:edit] do
+  #   redirect_to '/' unless is_admin? || is_current_user?
+  # end
+
+  # def index
+  #   @images = Image.all
+  # end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  Paperclip.options[:command_path] = "/usr/local/bin/"
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -38,17 +34,11 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
+  # Use an evented file watcher to asynchronously detect changes in source code, routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
