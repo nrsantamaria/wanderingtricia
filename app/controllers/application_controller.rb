ab3db438e367abc0a2e37a2824bd5304aba5d90d
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-07 10:01:54 -0700

    add flash errors to layouts and include navbar in application html. add devise helper with flash error messages. create application controller helper methods. load bootstrap in assets.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
+  helper_method :current_user
+
+  helper_method :is_admin?
+  helper_method :is_current_user?
+
+  def is_admin?
+    current_user && current_user.admin
+  end
+
+  def is_current_user?
+    if current_user
+      current_user.id == image.user_id
+    else
+      false
+    end
+  end
+
+# Custom parameters / Strong Parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+   protected
+
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+   end
+
 end
